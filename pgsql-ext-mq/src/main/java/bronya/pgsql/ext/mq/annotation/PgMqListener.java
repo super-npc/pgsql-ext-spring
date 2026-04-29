7fc85bce9ad660bf75507aa63bd454c18cb8773e
super-npc <super_npc@sina.cn>
2026-04-29 20:21:30 +0800

    feat:

--- pgsql-ext-mq/src/main/java/bronya/pgsql/ext/mq/annotation/PgMqListener.java
+++ pgsql-ext-mq/src/main/java/bronya/pgsql/ext/mq/annotation/PgMqListener.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 
+import bronya.pgsql.ext.mq.annotation.PgMqListener.SubscribeType;
+
 import java.io.Serializable;
 import java.lang.annotation.*;
 
@@ -19,7 +21,10 @@ public @interface PgMqListener {
     /** 绑定消息对象 */
     Class<? extends Serializable> bindMsgDto();
 
-    /** 可见性超时时间（秒） */
+    /** 可见性超时时间（秒） 
+     *消费者把消息 read/readWithPoll 拉出来以后，这条消息会在 VT 秒内对其他消费者“不可见” ，避免被并发重复消费；
+     如果在这段时间内没有被成功确认（这里是 delete ），VT 到期后消息会 再次变为可见 ，从而触发重投/重试。
+    */
     int visibilityTimeout() default 60;
 
     /** 批量拉取数量,多余的,目前没有想好怎么处理 */
